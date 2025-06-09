10b35db44ca5f6116d3332a7f1015031de5cdf24
dystrm <dystrm.dev@gmail.com>
2025-06-09 18:01:36 +0900

    뮤비 변경

--- crawler/config.py
+++ crawler/config.py
@@ -4,9 +4,9 @@ import os
 # 기본 정보
 TITLE = "안녕, 우주 (Memory)"
 ARTIST = "도영 (DOYOUNG)"
-VIDEO_ID = "Hf2wjEU2rzo"
+VIDEO_ID = "bpJS9ZnTkRw"
 DATA_DIR = "../js/data"
-DISCORD_ALERT_ENABLED = False
+DISCORD_ALERT_ENABLED = True
 
 # 비밀 키 경로 설정
 SECRETS_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../secrets"))
@@ -21,5 +21,3 @@ TWITTER_API = load_json("twitter_secrets.json")
 
 # Discord Webhook URL
 DISCORD_WEBHOOK_URL = load_json("discord_secrets.json")["webhook_url"]
-
-#test
