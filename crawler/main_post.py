f6d8367b93ea179bf34abe5d5aaa83fc04abdebc
dystrm <dystrm.dev@gmail.com>
2025-06-12 10:11:35 +0900

    [자동업데이트] 차트 데이터 갱신: 2025-06-12 10:11:35

--- crawler/main_post.py
+++ crawler/main_post.py
@@ -131,7 +131,7 @@ def main():
         push_to_github()
         return
 
-    elif now_hour in [0, 1]:
+    elif now_hour in [10, 11]:
         print(f"{now_hour}시: Playwright로 트윗 전송 시도")
         try:
             with open("tweet.txt", "w", encoding="utf-8") as f:
